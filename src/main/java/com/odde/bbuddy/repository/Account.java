a63f59a64980ea7dc81901e08d602f05b35d62b4
Jackson Zhang <zbcjackson@gmail.com>
2018-06-25 18:26:27 +0800

    Use javax NotBlank annotation

--- src/main/java/com/odde/bbuddy/repository/Account.java
+++ src/main/java/com/odde/bbuddy/repository/Account.java
@@ -3,10 +3,10 @@ package com.odde.bbuddy.repository;
 import com.odde.bbuddy.domain.Accounts;
 import com.odde.bbuddy.validator.Unique;
 import lombok.Data;
-import org.hibernate.validator.constraints.NotBlank;
 
 import javax.persistence.*;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Entity
