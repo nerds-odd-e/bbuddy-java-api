bca6f93e5fdcbddd96caa0137362367f5646daf7
zbcjackson <zbcjackson@gmail.com>
2022-06-16 22:12:48 +0800

    Add health check api

--- src/main/java/com/odde/bbuddy/config/WebSecurity.java
+++ src/main/java/com/odde/bbuddy/config/WebSecurity.java
@@ -36,6 +36,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/users/signup").permitAll()
+                .antMatchers(HttpMethod.GET, "/health").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager(), config))
---
+++ src/main/java/com/odde/bbuddy/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.odde.bbuddy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping
+    public void check() {}
+}
